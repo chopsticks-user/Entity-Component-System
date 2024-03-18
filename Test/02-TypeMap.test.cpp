1adcb21d1a4039664cd098d59cca3194430fc974
chopsticks-user <frostyfrost273@gmail.com>
2024-03-18 10:47:43 -0400

    TypeMap passed all tests

--- Test/02-TypeMap.test.cpp
+++ Test/02-TypeMap.test.cpp
@@ -16,7 +16,7 @@ u64 nTotalElements = 0;
 template <typename T> //
 void verifyAdd(u64 id) {
   REQUIRE(m.nTypes() == nTypes);
-  REQUIRE(m.nElements<T>() == nTotalElements);
+  REQUIRE(m.nElements() == nTotalElements);
   REQUIRE(m.exists<T>(id));
 }
 
@@ -47,8 +47,6 @@ TEST_CASE("Case #01: TypeMap.add", "[require]") {
   ++nTotalElements;
   verifyAdd<f64>(4);
 
-  nTotalElements += 3;
-
   m.add<cString>(0, "0.0");
   ++nTypes;
   ++nTotalElements;
@@ -67,73 +65,74 @@ TEST_CASE("Case #01: TypeMap.add", "[require]") {
 }
 
 TEST_CASE("Case #02: TypeMap.get", "[require]") {
-  //   REQUIRE(m.get<u64>(0) == 0);
-  //   REQUIRE(m.get<u64>(2) == 2);
-  //   REQUIRE(m.get<u64>(4) == 4);
+  REQUIRE(m.get<u64>(0) == 0);
+  REQUIRE(m.get<u64>(2) == 2);
+  REQUIRE(m.get<u64>(4) == 4);
 
-  //   REQUIRE(m.get<f64>(0) == 0.0);
-  //   REQUIRE(m.get<f64>(2) == 2.0);
-  //   REQUIRE(m.get<f64>(4) == 4.0);
+  REQUIRE(m.get<f64>(0) == 0.0);
+  REQUIRE(m.get<f64>(2) == 2.0);
+  REQUIRE(m.get<f64>(4) == 4.0);
 
-  //   REQUIRE(std::string{m.get<cString>(0)} == std::string{"0.0"});
-  //   REQUIRE(std::string{m.get<cString>(2)} == std::string{"2.0"});
-  //   REQUIRE(std::string{m.get<cString>(4)} == std::string{"3.0"});
+  REQUIRE(std::string{m.get<cString>(0)} == std::string{"3.0"});
+  REQUIRE(std::string{m.get<cString>(2)} == std::string{"2.0"});
+  REQUIRE(std::string{m.get<cString>(4)} == std::string{"4.0"});
 }
 
 template <typename T> //
 void verifyRemove(u64 id) {
-  //   REQUIRE(m.nTypes() == nTypes);
-  //   REQUIRE(m.nElements<T>() == nTotalElements);
-  //   REQUIRE_FALSE(m.exists<T>(id));
+  REQUIRE(m.nTypes() == nTypes);
+  REQUIRE(m.nElements() == nTotalElements);
+  REQUIRE_FALSE(m.exists<T>(id));
 }
 
 TEST_CASE("Case #03: TypeMap.remove", "[require]") {
-  //   m.remove<u64>(0);
-  //   --nTotalElements;
-  //   verifyRemove<u64>(0);
+  m.remove<u64>(0);
+  --nTotalElements;
+  verifyRemove<u64>(0);
 
-  //   m.remove<u64>(2);
-  //   --nTotalElements;
-  //   verifyRemove<u64>(2);
+  m.remove<u64>(2);
+  --nTotalElements;
+  verifyRemove<u64>(2);
 
-  //   m.remove<u64>(4);
-  //   --nTotalElements;
-  //   --nTypes;
-  //   verifyRemove<u64>(4);
-  //   REQUIRE(m.empty<u64>());
+  m.remove<u64>(4);
+  --nTotalElements;
+  m.remove<u64>();
+  --nTypes;
+  verifyRemove<u64>(4);
+  REQUIRE(m.empty<u64>());
 
-  //   m.remove<f64>(0);
-  //   --nTotalElements;
-  //   verifyRemove<f64>(0);
+  m.remove<f64>(0);
+  --nTotalElements;
+  verifyRemove<f64>(0);
 
-  //   m.remove<f64>(2);
-  //   --nTotalElements;
-  //   verifyRemove<f64>(2);
+  m.remove<f64>(2);
+  --nTotalElements;
+  verifyRemove<f64>(2);
 
-  //   m.remove<f64>(4);
-  //   --nTypes;
-  //   --nTotalElements;
-  //   verifyRemove<f64>(4);
-  //   REQUIRE(m.empty<f64>());
+  m.remove<f64>(4);
+  --nTotalElements;
+  m.remove<f64>();
+  --nTypes;
+  verifyRemove<f64>(4);
+  REQUIRE(m.empty<f64>());
 
-  //   nTotalElements += 3;
+  m.remove<cString>(0);
+  --nTotalElements;
+  verifyRemove<cString>(0);
 
-  //   m.remove<cString>(0);
-  //   --nTotalElements;
-  //   verifyRemove<cString>(0);
+  m.remove<cString>(2);
+  --nTotalElements;
+  verifyRemove<cString>(2);
 
-  //   m.remove<cString>(2);
-  //   --nTotalElements;
-  //   verifyRemove<cString>(2);
+  m.remove<cString>(2);
+  verifyRemove<cString>(2);
 
-  //   m.remove<cString>(2);
-  //   verifyRemove<cString>(2);
+  m.remove<cString>(4);
+  --nTotalElements;
+  m.remove<cString>();
+  --nTypes;
+  verifyRemove<cString>(4);
+  REQUIRE(m.empty<cString>());
 
-  //   m.remove<cString>(4);
-  //   --nTotalElements;
-  //   --nTypes;
-  //   verifyRemove<cString>(4);
-  //   REQUIRE(m.empty<cString>());
-
-  //   REQUIRE(m.empty());
+  REQUIRE(m.empty());
 }
