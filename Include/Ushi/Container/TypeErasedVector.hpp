077610451272cef799dd71187a0406d1c4e1cb24
chopsticks-user <frostyfrost273@gmail.com>
2024-04-21 19:33:02 -0400

    container::TypeErasedVector

--- Include/Ushi/Container/TypeErasedVector.hpp
+++ Include/Ushi/Container/TypeErasedVector.hpp
@@ -7,37 +7,83 @@ namespace ushi {
 namespace internal {
 namespace container {
 
-class TypeErasedVector {
-  //   using TElementShape = ;
+class TypeErasedVector final {
+public:
+  constexpr TypeErasedVector() noexcept = default;
 
-  template <u64 bytes> //
-  struct Element {
-    std::array<std::byte, bytes> data;
-  };
+  constexpr TypeErasedVector(size_t elementSize) noexcept
+      : m_elementSize{elementSize}, m_vector{} {}
 
 public:
-  TypeErasedVector() noexcept = default;
+  constexpr auto size() const noexcept -> size_t {
+    return m_vector.size() / m_elementSize;
+  }
 
-  constexpr TypeErasedVector(u64 elementSize)
-      : m_elementSize{elementSize},
-        m_sample{std::make_unique_for_overwrite<std::byte[]>(elementSize)},
-        m_data{} {}
+  constexpr auto elementSize() const noexcept -> size_t {
+    return m_elementSize;
+  }
 
-  constexpr auto size() const noexcept { return m_data.size() / m_elementSize; }
+  template <typename T> //
+  constexpr auto at(size_t index) -> T & {
+    m_throw_if_sizes_mismatch<T>();
+    m_throw_if_out_of_bounds(index);
+    return *reinterpret_cast<T *>(m_vector.data() + index * m_elementSize);
+  }
 
-  constexpr auto push(auto &&value) {
-    if (sizeof(value) != m_elementSize) {
+  template <typename T> //
+  constexpr auto at(size_t index) const -> T & {
+    m_throw_if_sizes_mismatch<T>();
+    m_throw_if_out_of_bounds(index);
+    return *reinterpret_cast<T *>(m_vector.data() + index * m_elementSize);
+  }
+
+  template <typename T> //
+  constexpr auto add(const T &value) -> void {
+    if (sizeof(T) != m_elementSize) {
       throw;
     }
-    // m_sample->s = reinterpret_cast<std::byte[]>(std::forward(value));
-    // m_data.insert
-    // std::swap_ranges();
+    const std::byte *pBytes = reinterpret_cast<const std::byte *>(&value);
+    m_vector.insert(m_vector.end(), pBytes, pBytes + m_elementSize);
+  }
+
+  template <typename T> //
+  [[nodiscard]] constexpr auto remove(size_t index) -> T {
+    m_throw_if_sizes_mismatch<T>();
+    if (m_out_of_bounds(index)) {
+      return {};
+    }
+
+    auto rmBegin = m_vector.begin() + index * m_elementSize;
+    auto vecEnd = m_vector.end();
+    std::swap_ranges(rmBegin, rmBegin + m_elementSize, vecEnd - m_elementSize);
+
+    T rmValue = std::move(*reinterpret_cast<T *>(
+        m_vector.data() + m_vector.size() - m_elementSize));
+    m_vector.erase(vecEnd - m_elementSize, vecEnd);
+    return rmValue;
   }
 
 private:
-  u64 m_elementSize = 1;
-  std::unique_ptr<std::byte[]> m_sample = nullptr;
-  std::vector<std::byte> m_data = {};
+  template <typename T> //
+  constexpr auto m_throw_if_sizes_mismatch() const -> void {
+    if (sizeof(T) != m_elementSize) {
+      throw std::runtime_error("m_throw_if_sizes_mismatch");
+    }
+  }
+
+  constexpr auto m_throw_if_out_of_bounds(size_t index) const -> void {
+    if (m_out_of_bounds(index)) {
+      throw std::runtime_error("m_throw_if_out_of_bounds");
+    }
+  }
+
+  constexpr auto m_out_of_bounds(size_t index) const -> bool {
+    return !(index * m_elementSize < m_vector.size());
+  }
+
+private:
+  size_t m_elementSize = 1;
+  std::vector<std::byte> m_vector = {};
 };
 
 } // namespace container
--- Include/Ushi/Impl/Group/GroupImpl.hpp
+++ Include/Ushi/Impl/Group/GroupImpl.hpp
@@ -5,8 +5,8 @@
 
 #include "Component/Component.hpp"
 #include "Config/Config.hpp"
+#include "Container/TypeErasedVector.hpp"
 #include "Entity/Entity.hpp"
-// #include "PolyTypeTable.hpp"
 
 namespace ushi {
 namespace internal {
@@ -14,8 +14,47 @@ namespace impl {
 
 template <IsConfig TConfig> //
 class Group {
+  using TCompatibleEntityIDs = std::vector<EntityID>;
+  using TArchetype =
+      std::unordered_map<ComponentRecordID, container::TypeErasedVector>;
+  using TArchetypeInfo = std::vector<std::pair<ComponentRecordID, u64>>;
+
 public:
+  constexpr Group() noexcept = default;
+
+  explicit constexpr Group(const TArchetypeInfo &archetypeInfo)
+      : m_entityIDs{}, m_archetype{} {
+    for (const auto &[componentID, elementSize] : archetypeInfo) {
+      m_archetype.try_emplace(componentID,
+                              container::TypeErasedVector{elementSize});
+    }
+  }
+
+  explicit constexpr Group(const TCompatibleEntityIDs &entityIDs,
+                           const TArchetype &archetype)
+      : m_entityIDs{entityIDs}, m_archetype{archetype} {}
+
+public:
+  constexpr auto level() const noexcept -> u64 { return m_archetype.size(); }
+
+  constexpr auto archetypeInfo() const noexcept -> TArchetypeInfo {
+    TArchetypeInfo info(m_archetype.size());
+    for (const auto &[componentID, componentVector] : m_archetype) {
+      info.push_back(
+          std::make_pair(componentID, componentVector.elementSize()));
+    }
+    return info;
+  }
+
+  template <IsComponent... TComponents> //
+  constexpr auto addEntity(const EntityID &entityID) -> void {
+    m_entityIDs.push_back(entityID);
+  }
+
 private:
+private:
+  TCompatibleEntityIDs m_entityIDs{};
+  TArchetype m_archetype = {};
 };
 
 } // namespace impl
